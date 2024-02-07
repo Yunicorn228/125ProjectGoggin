3a457730748ad26ed03f0cba82b75a605e6bc0d9
zixuy <zixuy@uci.edu>
2024-02-07 15:39:47 -0800

    logo set up

---
+++ Goggin/Goggin/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Goggin
+//
+//  Created by Zixu Yu on 2/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack{
+            Color(.black)
+                .ignoresSafeArea()
+            VStack{
+                Image("logo")
+                    .resizable()
+                    .cornerRadius(10)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.all)
+                Text("GOGIN")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.white)
+            }
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Goggin/Goggin/GogginApp.swift
@@ -0,0 +1,17 @@
+//
+//  GogginApp.swift
+//  Goggin
+//
+//  Created by Zixu Yu on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct GogginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
